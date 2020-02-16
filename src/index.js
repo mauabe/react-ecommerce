4522aba8812f630fdbca86ee047fd00711372435
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2020-02-15 23:31:14 -0800

    added redux-persistor to keep shopping cart in localstorage

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import{persistGate} from 'redux-persist/integration/react';
+
+import {store, persistor} from './redux/store';
 import App from './App';
 import './index.css';
 
@@ -10,7 +12,9 @@ import './index.css';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
