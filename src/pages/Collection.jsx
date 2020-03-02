bdc3271afe862ace11ff23f4b821e9c5bcd245ec
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2020-03-01 22:52:10 -0800

    adding category/collection to ShopPage

---
+++ src/pages/Collection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CollectionItem from '../components/CollectionItem';
+import '../scss/collection.scss';
+
+const CollectionPage = ({match}) => {
+  return (
+    <div className='collection'>
+      <h2>COLLECTION PAGE</h2>
+    </div>
+  )
+}
+
+export default CollectionPage;
