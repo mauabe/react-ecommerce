50b37dcd02ed942e234c4038aa6e612fd5352b17
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2020-03-08 17:53:00 -0700

    added COLLECTION_ID_MAP to select if number using url string

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
 import CollectionItem from '../components/CollectionItem';
 import '../scss/collection.scss';
+import {selectCollection} from '../redux/shop/shopSelector'
 
-const CollectionPage = ({match}) => {
+const CollectionPage = ({collection}) => {
+  console.log(collection)
   return (
-    <div className='collection'>
+    <div className='collection-page'>
       <h2>COLLECTION PAGE</h2>
-      {CollectionItem}
+      <CollectionItem />
     </div>
   )
 }
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
