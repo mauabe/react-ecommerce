7661a0ced14f84cd54cd34c797621e573ae12945
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2020-03-25 16:03:39 -0700

    added stripe button component

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import {selectCartItems, selectCartTotal} from '../redux/cart/cartSelector';
 import CheckoutItem from '../components/CheckoutItem';
+import StripeCheckoutButton from '../components/StripeButton'
 
 import '../scss/checkout.scss';
 
@@ -32,6 +33,12 @@ const CheckoutPage = ({cartItems, total}) => {
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className='test-warning'>
+     * please use the following test credit car for payments *
+     <br />
+     4242 4242 4242 4242 - Exp 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 )}
 
